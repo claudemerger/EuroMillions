0482847bf674832bb06985eac1627d6514de4e60
claudemerger <claude.merger@icloud.com>
2025-03-15 17:29:20 +0100

    Initial Commit

---
+++ EuroMillions/EuroMillionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EuroMillionsApp.swift
+//  EuroMillions
+//
+//  Created by Claude sur iCloud on 15/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct EuroMillionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EuroMillionsTests/EuroMillionsTests.swift
@@ -0,0 +1,17 @@
+//
+//  EuroMillionsTests.swift
+//  EuroMillionsTests
+//
+//  Created by Claude sur iCloud on 15/03/2025.
+//
+
+import Testing
+@testable import EuroMillions
+
+struct EuroMillionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
